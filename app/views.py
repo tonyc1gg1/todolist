1ba7bfa29cc8de0a803cd48c6af7f842215edeb9
tony <tony@gmail.com>
2025-05-07 21:36:58 +0800

    簡易語法測試

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import random
+
+# Create your views here.
+
+
+def lotto(request):
+    # 1~49 不重複六個數字跟排序
+    numbers = random.sample(range(1, 50), 6)
+    spec_number = random.randint(1, 49)
+    numbers = sorted(numbers)
+
+    # ["1","34","21",]
+    numbers = " ".join([str(i) for i in numbers])
+
+    # 將numbers串成字串輸出
+
+    result = {"numbers": numbers, "spec_number": spec_number}
+
+    return render(request, "lotto.html", result)
+
+
+def hello(request):
+    result = {"message": "測試!", "data": "123", "label": "文字"}
+
+    return JsonResponse(result)
