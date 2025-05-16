8d425ab6903eede0e20d9b87aec9935893aef0d4
tony <tony@gmail.com>
2025-05-16 21:38:42 +0800

    完成代辦事項更新跟刪除

--- todo/urls.py
+++ todo/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import todolist, viewtodo
+from .views import todolist, viewtodo, createtodo, deletetodo
 
 
 urlpatterns = [
     path("", todolist, name="todolist"),
     path("view/<int:id>/", viewtodo, name="viewtodo"),
+    path("create/", createtodo, name="createtodo"),
+    path("delete/<int:id>/", deletetodo, name="deletetodo"),
 ]
